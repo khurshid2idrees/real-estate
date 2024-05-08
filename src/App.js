4373f5bd9e91db58e948a8f599f5081e4334cc63
khurshid2idrees <umarkhurshid3@gmail.com>
2024-05-08 09:16:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { DarkModeProvider } from "./components/DarkModeContext";
+import Header from "./components/Header";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import PopularArea from "./sections/PopularArea";
+import Properties from "./sections/Properties";
+import Services from "./sections/Services";
+import Clients from "./sections/Clients";
+import Contact from "./sections/Contact";
+import { Footer } from "./components/Footer";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <DarkModeProvider>
+        <Header />
+        <Hero />
+        <About />
+        <PopularArea/>
+        <Properties/>
+        <Services/>
+        <Clients/>
+        <Contact/>
+        <Footer/>
+      </DarkModeProvider>
+    </> 
   );
-}
-
-export default App;
+};
---
+++ src/components/Footer.jsx
@@ -0,0 +1,127 @@
+import React, { useEffect } from "react";
+import { useDarkMode } from "../components/DarkModeContext";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaYoutube,
+  FaBuilding,
+  FaMobile,
+  FaFax,
+  FaArrowUp,
+  FaMoon,
+  Fasun,
+} from "react-icons/fa";
+import { Link } from "react-scroll";
+import { IoMdMail } from "react-icons/io";
+import prop7 from "../assets/images/prop7.jpg";
+import prop8 from "../assets/images/prop8.jpg";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+export const Footer = () => {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
+  return (
+    <>
+      <footer
+        className={`${
+          darkMode ? "dark bg-black" : "light bg-gray-800"
+        } w-full m-auto lg:px-20 px-10 py-20 grid lg:grid-cols-3 grid-cols-1 justify-center items-start lg:gap-20 gap-10`}
+      >
+        <div className="flex flex-col justify-center items-start gap-5">
+          <h1 className="text-white text-2xl font-semibold">About Us</h1>
+          <p className="text-slate-200 text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+            sequi voluptas eos dolorum ullam eius quibusdam, est atque
+            dignissimos ex?
+          </p>
+          <div
+            id="social-icons"
+            className="flex justify-start items-center gap-4 mt-4"
+          >
+            <div className="p-3 rounded-xl bg-white hover:bg-red-600 hover:text-white cursor-pointer transform hover:scale-110 transition-transform duration-300">
+              <FaFacebookF className="size-5" />
+            </div>
+            <div className="p-3 rounded-xl bg-white hover:bg-red-600 hover:text-white cursor-pointer transform hover:scale-110 transition-transform duration-300">
+              <FaInstagram className="size-5" />
+            </div>
+            <div className="p-3 rounded-xl bg-white hover:bg-red-600 hover:text-white cursor-pointer transform hover:scale-110 transition-transform duration-300">
+              <FaTwitter className="size-5" />
+            </div>
+            <div className="p-3 rounded-xl bg-white hover:bg-red-600 hover:text-white cursor-pointer transform hover:scale-110 transition-transform duration-300">
+              <FaYoutube className="size-5" />
+            </div>
+          </div>
+
+          <h1 className="text-white mt-8">
+            Copyright Real Estate, All Rights Reserved.
+          </h1>
+        </div>
+
+        <div className="flex flex-col justify-center items-start gap-5">
+          <h1 className="text-white text-2xl font-semibold">Contact Us</h1>
+          <div className="flex justify-center items-center gap-3">
+            <FaBuilding className="text-white size-5" />
+            <p className="text-slate-200">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+          <div className="flex justify-center items-center gap-3">
+            <FaMobile className="text-white size-5" />
+            <p className="text-slate-200">+974 2902 0290</p>
+          </div>
+          <div className="flex justify-center items-center gap-3">
+            <FaFax className="text-white size-5" />
+            <p className="text-slate-200">9494 76538</p>
+          </div>
+          <div className="flex justify-center items-center gap-3">
+            <IoMdMail className="text-white size-5" />
+            <p className="text-slate-200">demomail@gmail.com</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-center items-start gap-5">
+          <h1 className="text-white text-2xl font-semibold">
+            Latest Properties
+          </h1>
+          <div className="flex justify-center items-center gap-4">
+            <img
+              src={prop7}
+              alt=""
+              className="w-[120px] rounded-lg transform hover:scale-110 cursor-pointer transition-transform duration-300"
+            />
+            <div>
+              <h1 className="text-lg text-white">Villa with amazing view</h1>
+              <p className="text-slate-400">$ 287.98</p>
+            </div>
+          </div>
+          <div className="flex justify-center items-center gap-4">
+            <img
+              src={prop8}
+              alt=""
+              className="w-[120px] rounded-lg transform hover:scale-110 cursor-pointer transition-transform duration-300"
+            />
+            <div>
+              <h1 className="text-lg text-white">Smart view from beach</h1>
+              <p className="text-slate-400">$ 487.18</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+      {/* slide to top button */}
+
+      <div
+        id="icon-box"
+        className="bg-red-600 p-4 rounded-full hover:bg-black cursor-pointer fixed lg:bottom-12 bottom-6 right-6"
+      >
+        <Link to="hero" spy={true} offset={-100} smooth={true}>
+
+          <FaArrowUp className="size-6 text-white"/>
+        </Link>
+      </div>
+    </>
+  );
+};
